88f4d5d0de06d187e5b2c254fcd43a956db62ee3
Jarkko Heikkinen <jarkkohei@luukku.com>
2017-11-21 23:33:01 +0200

    Template ready

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  buttons = [
+    { name: '()', operator: true },
+    { name: '7', operator: false },
+    { name: '8', operator: false },
+    { name: '9', operator: false },
+    { name: '/', operator: true },
+
+    { name: '%', operator: true },
+    { name: '4', operator: false },
+    { name: '5', operator: false },
+    { name: '6', operator: false },
+    { name: '*', operator: true },
+
+    { name: '<', operator: true },
+    { name: '1', operator: false },
+    { name: '2', operator: false },
+    { name: '3', operator: false },
+    { name: '-', operator: true },
+
+    { name: 'C', warning: true },
+    { name: '0', operator: false },
+    { name: '.', operator: false },
+    { name: '=', operator: true },
+    { name: '+', operator: true }
+  ];
 }
